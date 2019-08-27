4de5cd5ddce429642c56495b089a9397ebe56af5
Ndagi Stanley <15629602+NdagiStanley@users.noreply.github.com>
2019-08-28 00:22:23 +0300

    Update url for TK

--- src/components/Sidebar/TK/TK.js
+++ src/components/Sidebar/TK/TK.js
@@ -6,7 +6,7 @@ const TK = () => (
     <pre>
       <p><b>You:</b> Hey, what's with the <b>.tk domain</b>?</p>
       <p><b>Me:</b> I'm glad you asked. Head over to
-        <a href="https://techkenyans.tk/" target="_blank">
+        <a href="https://techkenyans.org/" target="_blank">
           <img src="/tk.png"></img>
         </a>
         to find out!
